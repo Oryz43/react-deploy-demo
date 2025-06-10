bf20dd9f0fb103097f0f65d5413891b08938620c
Oryz43 <oriza0800@gmail.com>
2025-06-10 19:17:20 +0700

    mengganti h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App</h1>
+        <h1>Hallo selamat datang</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
